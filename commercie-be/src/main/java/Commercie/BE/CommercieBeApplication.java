6b5edea4bf930cfa14edfefc05f6adc249cc6d90
Shoru <bapcui596@gmail.com>
2024-04-02 23:11:26 +0700

    chore (BE): file clean up

---
+++ commercie-be/src/main/java/Commercie/BE/CommercieBeApplication.java
@@ -0,0 +1,13 @@
+package Commercie.BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercieBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercieBeApplication.class, args);
+	}
+
+}
---
+++ commercie-be/src/test/java/Commercie/BE/CommercieBeApplicationTests.java
@@ -0,0 +1,13 @@
+package Commercie.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercieBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
